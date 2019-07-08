9ad6c52c4436aa36a3c0f662ebbf04f4efa19e94
Amrit Shetra <1719349@brunel.ac.uk>
2019-07-08 18:05:12 +0100

    Sensor is able to track sound and print values to the console

--- sensors.py
+++ sensors.py
@@ -1,1 +1,22 @@
-print("Hello World!")
+# https://github.com/DexterInd/GrovePi/blob/master/Software/Python/grove_sound_sensor.py
+
+import time
+import grovepi
+
+# Connections
+sound_sensor = 0 # Port A0
+
+grovepi.pinMode(sound_sensor, "INPUT")
+
+while True:
+    try:
+        # Read the sound level
+        sensor_value = grovepi.analogRead(sound_sensor)
+        
+        print ("Sound sensor value = %d" %sensor_value)
+        time.sleep(0.5)
+    
+    except IOError:
+        print ("Error")
+
+
