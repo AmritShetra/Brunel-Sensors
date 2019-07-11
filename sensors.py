bf6e126169804bb13f9e06ff391d910cc147ee05
Amrit Shetra <1719349@brunel.ac.uk>
2019-07-11 17:44:09 +0100

    Added support for the Ultrasound Ranger and Red LED to detect if someone is in a room or not

--- sensors.py
+++ sensors.py
@@ -5,18 +5,39 @@ import grovepi
 
 # Connections
 sound_sensor = 0 # Port A0
+ultrasonic_ranger = 4 # Port D4
+red_led = 5 # Port D5
 
 grovepi.pinMode(sound_sensor, "INPUT")
+grovepi.pinMode(red_led, "OUTPUT")
+
+room_occupied = False
 
 while True:
     try:
         # Read the sound level
         sensor_value = grovepi.analogRead(sound_sensor)
         
+        # Read the distance
+        distance_value = grovepi.ultrasonicRead(ultrasonic_ranger)
+        
+        # Printing values, mainly for testing purposes
+        print (distance_value)
         print ("Sound sensor value = %d" %sensor_value)
+        
+        if distance_value < 500:
+            room_occupied = True
+        else:
+            room_occupied = False
+        
+        if room_occupied:
+            grovepi.digitalWrite(red_led, 1)
+            print ("Room is occupied")
+        else:
+            grovepi.digitalWrite(red_led, 0)
+        
         time.sleep(0.5)
     
     except IOError:
         print ("Error")
-
 
