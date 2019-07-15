a47962cedb508c1bfdf7971be1261970dc0a43fb
Amrit Shetra <1719349@brunel.ac.uk>
2019-07-15 17:30:37 +0100

    System now prints out the time at which the room is occupied and adds it to a list

--- sensors.py
+++ sensors.py
@@ -2,6 +2,10 @@
 
 import time
 import grovepi
+import numpy as np
+import matplotlib as plt
+from datetime import datetime
+import keyboard
 
 # Connections
 sound_sensor = 0 # Port A0
@@ -11,9 +15,12 @@ red_led = 5 # Port D5
 grovepi.pinMode(sound_sensor, "INPUT")
 grovepi.pinMode(red_led, "OUTPUT")
 
+program_active = True
 room_occupied = False
+printed = False # Check if datetime already printed while occupied
+datetimes_when_occupied = []
 
-while True:
+while program_active:
     try:
         # Read the sound level
         sensor_value = grovepi.analogRead(sound_sensor)
@@ -31,11 +38,17 @@ while True:
         if room_occupied:
             grovepi.digitalWrite(red_led, 1)
             print ("Room is occupied")
+            if printed == False:
+                now = datetime.now().time()
+                print(now)
+                datetimes_when_occupied.append(now)
+                printed = True
+
         else:
             grovepi.digitalWrite(red_led, 0)
+            printed = False
         
         time.sleep(0.5)
-    
+        
     except IOError:
         print ("Error")
-
